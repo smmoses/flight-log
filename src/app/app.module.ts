3c85271fa2a5c810b6aada9de0a562a1dde4f447
smmoses <scott.m.moses@gmail.com>
2021-07-22 17:09:06 -0500

    got fontawesome working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { ShellComponent } from './shell/shell.component';
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faSun } from '@fortawesome/free-solid-svg-icons';
+import { LogBookModule } from './log-book/log-book.module';
 
 @NgModule({
   declarations: [AppComponent, ShellComponent],
@@ -14,9 +17,17 @@ import { ShellComponent } from './shell/shell.component';
     BrowserAnimationsModule,
     CoreModule,
     AppRoutingModule,
+    FontAwesomeModule,
+    LogBookModule
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {
+  /**
+   *
+   */
+  constructor(library: FaIconLibrary) {
+    library.addIcons(faSun);
+  }
 }
--- src/app/log-book/log-book.module.ts
+++ src/app/log-book/log-book.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LogBookComponent } from './log-book.component';
 import { SharedModule } from '../shared/shared.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 
 
 @NgModule({
   declarations: [LogBookComponent],
-  imports: [CommonModule, SharedModule],
+  imports: [CommonModule, SharedModule, FontAwesomeModule],
 })
 export class LogBookModule {
 }
